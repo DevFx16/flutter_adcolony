278b9b3d90478593c9ba6aa0b4ee42f71f23498c
Fernando Araujo <developgadget@gmail.com>
2019-09-28 19:34:06 -0500

    Java Upgrade Plugin

--- android/src/main/java/com/developgadget/adcolony/AdcolonyPlugin.java
+++ android/src/main/java/com/developgadget/adcolony/AdcolonyPlugin.java
@@ -1,63 +1,29 @@
 package com.developgadget.adcolony;
-
-import android.app.Activity;
-import android.app.Application;
 import android.util.Log;
-
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import com.adcolony.sdk.*;
-
 import java.util.ArrayList;
 
 /** AdcolonyPlugin */
 public class AdcolonyPlugin implements MethodCallHandler {
 
-    private String APP_ID = null;
-    private String[] ZONE_IDS = null;
     private final Registrar registrar;
-    private final MethodChannel channel;
-    private AdColonyInterstitial AdInterstitial;
-    private AdColonyInterstitialListener listener;
+    private Interstitial AdInterstitial;
 
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "adcolony");
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "AdColony");
     channel.setMethodCallHandler(new AdcolonyPlugin(registrar, channel));
   }
 
     private AdcolonyPlugin(Registrar registrar, MethodChannel channel) {
         this.registrar = registrar;
-        this.channel = channel;
-        this.listener = new AdColonyInterstitialListener() {
-            @Override
-            public void onRequestFilled(AdColonyInterstitial ad) {
-                AdcolonyPlugin.this.AdInterstitial = ad;
-                AdcolonyPlugin.this.channel.invokeMethod("OnRequestFilled", null);
-                Log.d("ADCOLONY", "OnRequestFilled");
-            }
-
-            @Override
-            public void onRequestNotFilled(AdColonyZone zone) {
-                AdcolonyPlugin.this.channel.invokeMethod("OnRequestNotFilled", null);
-                Log.e("ADCOLONY", "OnRequestNotFilled");
-            }
-
-            @Override
-            public void onOpened(AdColonyInterstitial ad) {
-                AdcolonyPlugin.this.channel.invokeMethod("OnOpened", null);
-                Log.d("ADCOLONY", "OnOpened");
-            }
-
-            @Override
-            public void onExpiring(AdColonyInterstitial ad) {
-                AdcolonyPlugin.this.channel.invokeMethod("OnExpiring", null);
-                Log.d("ADCOLONY", "OnExpiring");
-            }
-        };
+        this.AdInterstitial = new Interstitial(channel);
+        AdColony.setRewardListener(this.AdInterstitial);
     }
 
     @Override
@@ -67,72 +33,32 @@ public class AdcolonyPlugin implements MethodCallHandler {
                 callInitialize(call, result);
                 break;
             case "loadInterstitial":
-                callLoadInterstitial(call, result);
+                this.AdInterstitial.Request((String) call.argument("ZONE_ID"));
                 break;
             case "showAd":
-                callShowAd(call, result);
-                break;
-            case "loadRewarded":
-                callLoadRewarded(call, result);
+                this.AdInterstitial.Show();
                 break;
             default:
                 result.notImplemented();
                 break;
         }
-        return;
+        result.success(Boolean.TRUE);
     }
 
     private void callInitialize(MethodCall call, Result result) {
-        this.APP_ID = call.argument("APP_ID");
-        ArrayList e = call.argument("ZONE_IDS");
-        this.ZONE_IDS = new String[e.size()];
-        for(int i = 0; i < e.size(); i++){
-            this.ZONE_IDS[i] = e.get(i).toString();
-        }
-        if (this.APP_ID == null || this.APP_ID.isEmpty() || this.ZONE_IDS == null || this.ZONE_IDS.length == 0) {
-            result.error("NO APP_ID or ZONE_IDS", "a null or empty APP_ID or ZONE_IDS was provided", null);
-            return;
-        }
-        AdColony.configure(registrar.activity(), this.APP_ID, this.ZONE_IDS);
-        result.success(Boolean.TRUE);
-    }
-
-    private void callLoadInterstitial(MethodCall call, Result result) {
-        String ZoneId = call.argument("ZONE_ID");
-        if (!ZoneId.isEmpty()) {
-            if (this.AdInterstitial == null || this.AdInterstitial.isExpired()) {
-                AdColony.requestInterstitial(ZoneId, listener, new AdColonyAdOptions());
+        String APP_ID = call.argument("APP_ID");
+        ArrayList ListZONE = call.argument("ZONE_IDS");
+        if(ListZONE != null){
+            String[] ZONE_IDS = new String[ListZONE.size()];
+            for(int i = 0; i < ListZONE.size(); i++){
+                ZONE_IDS[i] = ListZONE.get(i).toString();
             }
-        } else {
-            Log.d("ADCOLONY ZONE_ID", "ZONE_ID IS EMPTY");
-        }
-        result.success(Boolean.TRUE);
-    }
-
-    private void callShowAd(MethodCall call, Result result) {
-        if (this.AdInterstitial != null && !this.AdInterstitial.isExpired()) {
-            this.AdInterstitial.show();
-        } else {
-            Log.d("ADCOLONY Ad", "Ad not request");
-        }
-        result.success(Boolean.TRUE);
-    }
-
-    private void callLoadRewarded(MethodCall call, Result result) {
-        String ZoneId = call.argument("ZONE_ID");
-        if (ZoneId.isEmpty()) {
-            Log.d("ADCOLONY ZONE_ID", "ZONE_ID IS EMPTY");
-        } else {
-            if (this.AdInterstitial == null || this.AdInterstitial.isExpired()) {
-                AdColony.setRewardListener(new AdColonyRewardListener() {
-                    @Override
-                    public void onReward(AdColonyReward reward) {
-                        AdcolonyPlugin.this.channel.invokeMethod("OnReward", null);
-                        Log.d("Ad Reward", "OnReward");
-                    }
-                });
-                AdColony.requestInterstitial(ZoneId, listener, new AdColonyAdOptions());
+            if (APP_ID == null || APP_ID.isEmpty()  || ZONE_IDS.length == 0) {
+                result.error("NO APP_ID or ZONE_IDS", "a null or empty APP_ID or ZONE_IDS was provided", null);
+                return;
             }
+            AdColony.configure(registrar.activity(), APP_ID, ZONE_IDS);
         }
+        result.success(Boolean.TRUE);
     }
 }
---
+++ android/src/main/java/com/developgadget/adcolony/Interstitial.java
@@ -0,0 +1,59 @@
+package com.developgadget.adcolony;
+
+import android.util.Log;
+
+import com.adcolony.sdk.AdColony;
+import com.adcolony.sdk.AdColonyInterstitial;
+import com.adcolony.sdk.AdColonyInterstitialListener;
+import com.adcolony.sdk.AdColonyReward;
+import com.adcolony.sdk.AdColonyRewardListener;
+import com.adcolony.sdk.AdColonyZone;
+
+import io.flutter.plugin.common.MethodChannel;
+
+class Interstitial extends AdColonyInterstitialListener implements AdColonyRewardListener {
+
+    private AdColonyInterstitial Ad;
+    private final MethodChannel channel;
+
+    Interstitial(MethodChannel channel) {
+        this.channel = channel;
+    }
+
+    void Show() {
+        if (this.Ad != null)
+            Ad.show();
+        else
+            Log.d("AdColony", "Ad not request");
+
+    }
+
+    void Request(String ZoneId) {
+        if (ZoneId != null && !ZoneId.isEmpty()) {
+            if (this.Ad == null || this.Ad.isExpired()) {
+                AdColony.requestInterstitial(ZoneId, this);
+            }
+        } else {
+            Log.d("AdColony", "ZONE_ID IS EMPTY");
+        }
+    }
+
+    @Override
+    public void onRequestFilled(AdColonyInterstitial adColonyInterstitial) {
+        this.Ad = adColonyInterstitial;
+        this.channel.invokeMethod("onRequestFilled", null);
+        Log.e("AdColony", "onRequestFilled");
+    }
+
+    @Override
+    public void onRequestNotFilled(AdColonyZone zone) {
+        this.channel.invokeMethod("onRequestNotFilled", null);
+        Log.e("AdColony", "onRequestNotFilled");
+    }
+
+    @Override
+    public void onReward(AdColonyReward adColonyReward) {
+        this.channel.invokeMethod("onReward", null);
+        Log.e("AdColony", "onReward");
+    }
+}
