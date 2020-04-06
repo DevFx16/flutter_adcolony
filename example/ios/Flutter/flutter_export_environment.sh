52bd30481b16e341d7aae3ae8589095e41195306
DevelopGadget <DevelopGadget@gmail.com>
2020-04-06 09:16:33 -0500

    Readme

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\ferna\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ferna\Documents\Projects\adcolony\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ferna\Documents\Projects\flutter_adcolony\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
